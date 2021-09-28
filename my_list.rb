75a6d3906754018423564d6ecc76c5f964ae5344
Usman Suleiman <usmansbk@gmail.com>
2021-09-28 10:48:09 +0100

    Remove test

--- my_list.rb
+++ my_list.rb
@@ -5,13 +5,3 @@ class MyList < MyEnumerable
     @list = list
   end
 end
-
-my_list = MyList.new(1, 2, 3, 4)
-
-my_list.all? { |e| e < 5 }
-my_list.all? { |e| e > 5 }
-
-my_list.any? { |e| e == 2 }
-my_list.any? { |e| e == 5 }
-
-my_list.filter { |e| e.even? }
