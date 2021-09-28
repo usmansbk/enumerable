d6e794ad419f05fd027e4e7aace4993edf472723
Usman Suleiman <usmansbk@gmail.com>
2021-09-28 14:58:19 +0100

    Remove self

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
   def all?
-    self.each { |num| return false unless yield num }
+    each { |num| return false unless yield num }
     true
   end
 
   def any?
-    self.each { |num| return true if yield num }
+    each { |num| return true if yield num }
     false
   end
 
   def filter
     filtered = []
-    self.each { |num| filtered.push(num) if yield num }
+    each { |num| filtered.push(num) if yield num }
     filtered
   end
 end
