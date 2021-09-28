fe43c4acf2fca0c1c903eedfbfc2058ef1a4a8bf
Usman Suleiman <usmansbk@gmail.com>
2021-09-28 14:55:58 +0100

    Implement each method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
   def all?
-    @list.each { |num| return false unless yield num }
+    self.each { |num| return false unless yield num }
     true
   end
 
   def any?
-    @list.each { |num| return true if yield num }
+    self.each { |num| return true if yield num }
     false
   end
 
   def filter
     filtered = []
-    @list.each { |num| filtered.push(num) if yield num }
+    self.each { |num| filtered.push(num) if yield num }
     filtered
   end
 end
--- my_list.rb
+++ my_list.rb
@@ -6,4 +6,12 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+  end
 end
